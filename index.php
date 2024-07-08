ddc8bb4f1d5c345c8ee7e9464773a7fdec0f0d49
KomalHiremath17 <komalnvh17@gmail.com>
2024-07-08 20:24:39 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,191 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>EXPLORE TRAVELS</title>
+   <link rel="shortcut icon" href="images.png" type="image/x-icon">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
+     <style>
+      @import url('https://fonts.googleapis.com/css2?family=Caveat&display=swap');
+       #rr{
+        width: 100px;
+       }
+      #rr1{
+        width: 30px;
+        
+      }
+      body{
+        text-decoration: none;
+        font-family: 'Courier New', Courier, monospace;
+      }
+      footer{
+        position:relative;
+      }
+      .text_title{
+        font-family: "Trebuchet MS",Sans-serif;
+    color: #046BBA;
+    font-size:50px;
+    text-align:center;
+      }
+    #sr{
+      width: auto;
+      height: 600px;
+    }
+
+    .container {
+  position: relative;
+  text-align: center;
+  color: white;
+}
+    .centered {
+      font-family: 'Caveat', cursive;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
+#tt1{
+height:500px;
+}
+   
+  
+     </style>
+
+</head>
+<body>
+
+
+
+<nav style="" class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="index.php"><img id="rr" src="images.png" alt=""></a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div class="navbar-nav">
+        <a class="nav-link active" aria-current="page" href="index.php">|Home|</a>
+        <a class="nav-link active " href="about.php">|about us|</a>
+        <a class="nav-link active" href="packages.php">|packages|</a>
+        <a class="nav-link active" href="blogs.php">|blogs|</a>
+        <a class="nav-link active " href="contact.php">|contact us|</a>
+       <a class="nav-link active" href="contact.php" > <button type="button" class="btn btn-dark">GET A QUOTE </button></a>
+      </div>
+    </div>
+  </div>
+</nav>
+
+
+
+
+
+
+<div id="carouselExampleCaptions" class="carousel slide">
+  <div class="carousel-indicators">
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+  </div>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img id="tt1" src="gold.jpeg" class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <h1>YOUR JOURNEY BIGINS</h1>
+        <p>It is time to travel and explore. "The sky has no limits; keep flying." explore Travel has several deals and packages for you. Follow us on Instagram, for more updates: </p>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img id="tt1" src="tower.jpeg" class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <h1>Book your tickets explore travels
+        </h1>
+        <p>specialize in reasonable air tickets to India from different country's. We are just a call away.@Exploretravels </p>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img id="tt1" src="dubai.jpeg" class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <h1>pack your bags</h1>
+        <p>Relax and book a holiday for yourself. We will take care of your travel needs. 
+
+</p>
+      </div>
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+
+
+
+<br>
+
+<div style="text-align:center;  color: #046BBA; font-family: 'Trebuchet MS',Sans-serif;" class="sub_title second_font">Live your life </div>
+<div class="text_title">Explore the world and relax.</div>
+<div class="card ">
+		<img decoding="async"  
+     src="https://dhindsatravels.com/wp-content/uploads/2022/07/h1-img-1.png" 
+     class="attachment-full size-full wp-image-6659" alt="Google map" srcset="https://dhindsatravels.com/wp-content/uploads/2022/07/h1-img-1.png 1523w, 
+     https://dhindsatravels.com/wp-content/uploads/2022/07/h1-img-1-300x145.png 300w, https://dhindsatravels.com/wp-content/uploads/2022/07/h1-img-1-1024x496.png 1024w,
+     https://dhindsatravels.com/wp-content/uploads/2022/07/h1-img-1-768x372.png 768w, https://dhindsatravels.com/wp-content/uploads/2022/07/h1-img-1-600x291.png 600w" 
+    sizes="(max-width: 1523px) 100vw, 1523px">		
+                          
+                          
+                          
+  </div>
+
+<br>
+
+
+
+  <div class="container">
+
+  <img src="dl.jpeg" alt="Snow" style="width:100%;">
+ 
+  <div class="centered"><h2>Best offers for you!</h2>
+  <h1>Airline Ticket Prices Have Never Been This Low. Book Now or Regret It Later.</h1>
+ <a href="contact.php"> <button type="button" class="btn btn-secondary"><h2>contact us</h2></button></a>
+</div>
+</div>
+
+
+
+
+
+
+<br>
+
+<footer >
+<div class="shadow p-3 mb-5 bg-body-tertiary rounded">
+  <div class="row align-items-start">
+    <div class="col">
+      <strong >location </strong>
+    </br>
+      <p><img id="rr1" src="download.png" alt="">karnatak,india</p>
+      
+    </div>
+    <div class="col">
+      <strong>phone </strong>
+      <p>8660710***</p>
+      <p>7296366***</p>
+        </div>
+    <div class="col">
+      <strong>contact us </strong>
+     <a href="https://www.instagram.com/accounts/login/" style="text-decoration:none;"><p><img id="rr1" src="insta.jpeg" alt="">instagram</p></a>
+     <a href="https://www.facebook.com/accounts/login/" style="text-decoration:none;"><p><img id="rr1" src="face.png" alt="">facebook</p></a>
+    </div>
+ </div>
+</div>
+
+</footer>
+</body>
+</html>
